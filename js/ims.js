0e86f4ec4cc4c3c4fec1fbed59a3dc67279fe560
siva <sivaprakasam_sundaram@compaid.co.in>
2018-11-28 14:59:12 +0530

    Added common file for left menu-moved log out icon to left-added new menus for POs

---
+++ js/ims.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    //$(".page-sidebar ul li").each(function () {
+    //    if ($(this).baseURI == "rfq-index.html")
+    //        $(this).addClass("active");
+    //})
+
+    $(function () {
+        var pgurl = window.location.pathname.replace("/","");
+        $(".page-sidebar ul li").each(function () {
+            if ($('a', this).attr("href") == pgurl)
+                $(this).addClass("active");
+        })
+    });
+
+});
